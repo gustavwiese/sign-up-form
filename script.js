5f14ff07205af9e4232dc21e8ecd282c6c9a6b4d
gustavwiese <gusse12345@gmail.com>
2023-04-20 11:26:16 +0200

    added first js functions

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-"use strict"
+"use strict";
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("JavaScript kører");
+  document.querySelector("form#signup").addEventListener("submit", clickSubmit);
+}
+
+function clickSubmit(event) {
+  event.preventDefault();
+  console.log("Click Submit");
+  const elements = document.querySelector("form#signup").elements;
+  const signup = {
+    fullname: elements.namedItem("fname").value,
+    email: elements.namedItem("email").value,
+    username: elements.namedItem("username").value,
+    password: elements.namedItem("password").value,
+    payment: elements.namedItem("payment").value,
+    newsletter: elements.namedItem("emailYes").value,
+    terms: elements.namedItem("terms").value,
+  };
+  console.log(signup);
+}
